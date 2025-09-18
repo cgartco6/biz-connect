28c222bcb1bfc8932dde109dc45ea78c226e8e73
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-09-18 11:00:05 +0200

    Create init.py

---
+++ app/ai_team/init.py
@@ -0,0 +1,17 @@
+from app.ai_team.architect import Architect
+from app.ai_team.accountant import Accountant
+from app.ai_team.concierge import Concierge
+from app.ai_team.curator import Curator
+from app.ai_team.sentinel import Sentinel
+from app.ai_team.analyst import Analyst
+from app.ai_team.trainer import Trainer
+
+__all__ = [
+    'Architect',
+    'Accountant',
+    'Concierge',
+    'Curator',
+    'Sentinel',
+    'Analyst',
+    'Trainer'
+]
