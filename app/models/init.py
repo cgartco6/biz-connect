0176c2884cdcf0d0a8d9a0c411a39ff5f82207e0
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-09-18 10:55:58 +0200

    Create init.py

---
+++ app/models/init.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.business import Business, Town
+from app.models.payment import Payment, SubscriptionPlan
+
+__all__ = ['User', 'Business', 'Town', 'Payment', 'SubscriptionPlan']
